0f61615f24d9ff9c132bcbb563a6a88382784958
samuelahmed <samueladuval@gmail.com>
2022-01-16 16:47:14 -0800

    updated flexboxes homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,48 +12,157 @@
 
 
     <div id="q-app">
-      <div class="q-pa-md row items-start q-gutter-md flex-center" style="height: 250px; margin-top: 0px">
-    <q-card
+      <div class="q-pa-md row items-start q-gutter-md flex-center">
+
+
+
+
+
+
+
+<q-card
       class="my-card text-black"
-      style="background: radial-gradient(circle, #FFD700 0%, #FFFFFF 100%)"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
     >
       <q-card-section>
-        <div class="text-h6">Foundation</div>
+        <div class="text-h6">What’s a Blockchain</div>
+        <div class="text-subtitle2">The Holy Grail</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
+      </q-card-section>
+       </q-card>
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">How does it work</div>
+        <div class="text-subtitle2">The most Beautiful Code</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+Bitcoin showed that it is possible to use computing power to solve social issues. Some issues, such as digital ownership and currencies, may seem solved at first glance as most of us use some sort of online banking, make digital payments, and our personal investments are often done on purely digital platforms, but technological decisions have major implications for the distribution of power across the society. Whoever owns the servers owns the servers ultim   
+</q-card-section>
+       </q-card>
+
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Ledgers are Important</div>
+        <div class="text-subtitle2">World of Money</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+The Crypto Ancients foundation is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you make no excuses and devote your life to fighting greed and evil.The Crypto Ancients foundation is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you make no excuses and devote your life to fighting greed and evil.
+
+      </q-card-section>
+       </q-card>
+
+
+
+
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Bitcoin and Ethereum</div>
+        <div class="text-subtitle2">The Digital Empires</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+A non-fungible token (NFT) is a series in which each member is different. Humans are non-fungible members of the homo sapiens series, whereas the US dollar bills are fungible because can be exchanged equally for one another. The homo sapient or felis catus series are backed by methods of biology (decentralized), the US dollar is backed by the government (centralized). NFTs are digital tokens backed by new decentralized authorities known as blockchains. 
+      </q-card-section>
+       </q-card>
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Where do NFTs fit?</div>
+        <div class="text-subtitle2">Method for Digital Ownership</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
+</q-card-section>
+       </q-card>
+
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">What's a Crypto Ancient</div>
         <div class="text-subtitle2">Nodes of Truth</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-        The Crypto Ancients foundation is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you make no excuses and devote your life to fighting greed and evil.
+Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
+      </q-card-section>
+       </q-card>
+
+
+
+
+
+
+    <q-card
+      class="my-card text-black"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+    >
+      <q-card-section>
+        <div class="text-h6">Decentralization vs Centralization</div>
+        <div class="text-subtitle2">Perpetual Pendulum</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+A non-fungible token (NFT) is a series in which each member is different. Humans are non-fungible members of the homo sapiens series, whereas the US dollar bills are fungible because can be exchanged equally for one another. The homo sapient or felis catus series are backed by methods of biology (decentralized), the US dollar is backed by the government (centralized). NFTs are digital tokens backed by new decentralized authorities known as blockchains. 
       </q-card-section>
        </q-card>
 
 
     <q-card
       class="my-card text-black"
-      style="background: radial-gradient(circle, #596EBF 0%, #FFFFFF 100%)"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
     >
       <q-card-section>
-        <div class="text-h6">Origin</div>
-        <div class="text-subtitle2">Web3 Pioneers</div>
+        <div class="text-h6">Getting Involved </div>
+        <div class="text-subtitle2">Architects of the Future</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-Inspired by CryptoPunks, Nouns, and other pioneering projects, Crypto Ancients are resurrected to enjoy the sun with us, their decendents. A collection of 10,000 NFTs hand-drawn from the beyond.
-      </q-card-section>
+Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
+</q-card-section>
        </q-card>
 
 
 
     <q-card
       class="my-card text-black"
-      style="background: radial-gradient(circle, #0072C5 0%, #FFFFFF 100%)"
+      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
     >
       <q-card-section>
-        <div class="text-h6">Hope</div>
-        <div class="text-subtitle2">NFTs for digital ownership</div>
+        <div class="text-h6">Enter the Metaverse</div>
+        <div class="text-subtitle2">Clean Spaces</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-        digital ownership is important because... 
+Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
       </q-card-section>
        </q-card>
+
+
+
+
+
+
+
 
 
      </div>
@@ -110,6 +219,5 @@ export default defineComponent({
   width: 100%
   height: 100%
   max-width: 350px
-  max-height: 350px
   
 </style>
