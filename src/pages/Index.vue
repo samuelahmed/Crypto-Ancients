27eaee2984f3846c13718736f4096419d2ff64ae
samuelahmed <samueladuval@gmail.com>
2022-01-16 23:12:26 -0800

    c

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,78 +1,61 @@
 <template>
-
-<div id="background">
-  
- <q-page class="flex flex-center"> 
-    
+  <div id="background">
+    <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
       src="~assets/nftHumans.png"
       style="width: 400px; height: 400px"
-    /> 
-
-
-
-    <div id="q-app">
-
- <div class="q-pa-md row items-start q-gutter-md flex-center">
-<q-card
+      /> 
+  <div id="q-app">
+    <div class="q-pa-md row items-start q-gutter-md flex-center" style="height: 380px">
+      <q-card
       class="my-card text-black"
       style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
-    >
-      <q-card-section>
+      >
+     <q-card-section>
         <div class="text-h6">What’s a Blockchain</div>
         <div class="text-subtitle2">Emerging Digital States</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
+      Simply put a blockchain is a distributed digital ledger, a record of IDs and their assets that is maintained by many full-nodes globally. The revolution ushered by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries. Blockchains achieve this without a central authority by instead relying on cryptographic proofs. 
       </q-card-section>
+      </q-card>
+
+      <q-card
+       class="my-card text-black"
+       style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
+       > 
+        <q-card-section>
+          <div class="text-h6">How does it work</div>
+           <div class="text-subtitle2">Injecting Cryptographic Trust</div>
+        </q-card-section>
+        <q-card-section class="q-pt-none">
+        Blockchains are a method to validate the exchanges on a distributed ledger. Each block is tied to a specific value that is created by running the data within through a cryptographic hash. Any change to the block will change that value wildly, so just by knowing that number we can be certain that past data has not been altered. Solving other cryptographic puzzles we can decide which miner can post a block and be rewarded. In other words we are building a structure of data with properties that improve upon human trust. 
+        </q-card-section>
        </q-card>
 
-
-    <q-card
+      <q-card
       class="my-card text-black"
       style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
-    >
-      <q-card-section>
-        <div class="text-h6">How does it work</div>
-        <div class="text-subtitle2">Injecting Cryptographic Trust</div>
-      </q-card-section>
-      <q-card-section class="q-pt-none">
-Bitcoin showed that it is possible to use computing power to solve social issues. Some issues, such as digital ownership and currencies, may seem solved at first glance as most of us use some sort of online banking, make digital payments, and our personal investments are often done on purely digital platforms, but technological decisions have major implications for the distribution of power across the society. Whoever owns the servers owns the servers ultim   
-</q-card-section>
-       </q-card>
-
-
-
-    <q-card
-      class="my-card text-black"
-      style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
-    >
+      >
       <q-card-section>
         <div class="text-h6">Ledgers are Important</div>
         <div class="text-subtitle2">World of Money</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-The Crypto Ancients foundation is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you make no excuses and devote your life to fighting greed and evil.The Crypto Ancients foundation is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you make no excuses and devote your life to fighting greed and evil.
-
+        Ledgers are one of the foundations of capitalism. They allow for enterprise at scale, borrowing and loaning, the standardization of money exchanges and the ability to do so quickly securely without having to carry lots of valuable goods, such as precious metals or cash. For ledgers to work they need to be backed by some sort of authority so that the users know they can rely on what is writen. The beauty of decentralized cryptographic ledgers is that they allow all parties to rely on crytographic proofs instead of human-institutiosn to create turst. 
       </q-card-section>
-       </q-card>
-
-
-
-
-
-
-    <q-card
+      </q-card>
+      <q-card
       class="my-card text-black"
       style="background: radial-gradient(circle, #FFFFFF 0%, #FFFFFF 100%)"
     >
       <q-card-section>
-        <div class="text-h6">Bitcoin and Ethereum</div>
-        <div class="text-subtitle2">Digital Empires</div>
+        <div class="text-h6">Are Banks just Rent Seeking?</div>
+        <div class="text-subtitle2">Our Grandparents Landlord</div>
       </q-card-section>
       <q-card-section class="q-pt-none">
-A non-fungible token (NFT) is a series in which each member is different. Humans are non-fungible members of the homo sapiens series, whereas the US dollar bills are fungible because can be exchanged equally for one another. The homo sapient or felis catus series are backed by methods of biology (decentralized), the US dollar is backed by the government (centralized). NFTs are digital tokens backed by new decentralized authorities known as blockchains. 
+Somewhere between 5-10% of the world’s economy is given to banks. In exchange they allow commerce at scale; international money exchanges, acts as a trusted third party, loans, basic financial services, and more. Without the internet this price tag is a bargain. However, in today’s world it is about 3-10% too high. More so, decades of clear disregard for structural stability, basic economic security, high fees to users, and constant predatory practices make a strong case for investments in alternatives. 
       </q-card-section>
        </q-card>
 
