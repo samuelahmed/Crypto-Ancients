0662cdfbccde31d9ea9a83a063ce8c88d9e35376
samuelahmed <samueladuval@gmail.com>
2022-01-18 13:54:39 -0800

    c

--- src/components/ImgComponent.vue
+++ src/components/ImgComponent.vue
@@ -1,14 +1,20 @@
 <template>
 
-              <q-img
+    <q-btn label="{{ edition }}" @click="card = true">
+
+         <q-img 
+
         :src="`https://gateway.pinata.cloud/ipfs/QmVuuRsSJRB4REFA9jgfyJzEPBnJU4qwW3tdgZHNUWcKMN/${edition}.png`"
         spinner-color="red"
         style="height: 69px; max-width: 69px; margin: 5px"
-      />
+        class ="responsive">
+
+</q-img>
+      </q-btn>
+
 </template>
 
 <script>
-
 
 export default {
     name: 'imgComp',
@@ -23,5 +29,6 @@ export default {
         compiler: String
     }  
 }
+
 
 </script>
