a2de4332aed3f9476677071f04727f32a3b86909
samuelahmed <samueladuval@gmail.com>
2022-02-06 17:07:30 -0800

    c

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,10 @@
 <template>
-
-
-<div id="q-app" style="min-height: 100vh;">
-  <div class="background-image">
-
- 
-
-
+<div class="sam-image">
+<div id="q-app">
   <div class="text-h6 flex flex-center" style="width: 1000px; height: 100px">
   A Story of Blockchains and NFTs [1/19]
   </div>
 <div class="row items-start q-gutter-md flex-center" style="height: 300px">
-
 
 <q-card
   class="my-card text-black"
@@ -21,7 +14,7 @@
   <div class="text-subtitle2">1</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-A blockchain is a distributed digital ledger, a record of IDs and their assets, that is maintained by global peer to peer networks. 
+  A blockchain is a distributed digital ledger, a record of IDs and their assets, that is maintained by global peer to peer networks. 
   </q-card-section>
 </q-card>
 
@@ -33,7 +26,7 @@ A blockchain is a distributed digital ledger, a record of IDs and their assets, 
   <div class="text-subtitle2">2</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-The revolution started by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries.
+  The revolution started by Bitcoin is the ability to turn computing power into social trust. So that all participants in the network can be assured that the ledger is most likely accurate even in the face of adversaries.
   </q-card-section>
 </q-card>
 
@@ -45,7 +38,7 @@ The revolution started by Bitcoin is the ability to turn computing power into so
   <div class="text-subtitle2">3</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Blockchains achieve this security without a central authority by using cryptographic proofs which leverage known deterministic patterns of nature to solve problems such as double spending.
+  Blockchains achieve this security without a central authority by using cryptographic proofs which leverage known deterministic patterns of nature to solve problems such as double spending.
   </q-card-section>
 </q-card>
 
@@ -57,7 +50,7 @@ Blockchains achieve this security without a central authority by using cryptogra
   <div class="text-subtitle2">4</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-When completed each block has a set value.  Any change to data in the block will vary that value wildly, so just by knowing that number we can also have trust that past data has not been altered.
+  When completed each block has a set value.  Any change to data in the block will vary that value wildly, so just by knowing that number we can also have trust that past data has not been altered.
   </q-card-section>
 </q-card>
 
@@ -69,7 +62,7 @@ When completed each block has a set value.  Any change to data in the block will
   <div class="text-subtitle2">5</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Ledgers are one of the foundations of capitalism. They allow for enterprise at scale, borrowing and loaning, the standardization of money exchanges and the ability to do so quickly securely without having to carry lots of valuable goods, such as precious metals or cash. 
+  Ledgers are one of the foundations of capitalism. They allow for enterprise at scale, borrowing and loaning, the standardization of money exchanges and the ability to do so quickly securely without having to carry lots of valuable goods, such as precious metals or cash. 
   </q-card-section>
 </q-card>
 
@@ -81,7 +74,7 @@ Ledgers are one of the foundations of capitalism. They allow for enterprise at s
   <div class="text-subtitle2">6</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-For ledgers to work they need to be backed by some sort of authority so that the users know they can rely on what is written.  In the past this was corporations and countries, now blockchains have entered the sphere. 
+  For ledgers to work they need to be backed by some sort of authority so that the users know they can rely on what is written.  In the past this was corporations and countries, now blockchains have entered the sphere. 
   </q-card-section>
 </q-card>
 
@@ -93,7 +86,7 @@ For ledgers to work they need to be backed by some sort of authority so that the
   <div class="text-subtitle2">7</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Somewhere between 5-10% of the world’s economy is given to banks. In exchange they allow commerce at scale; international money exchanges, acts as a trusted third party, loans, basic financial services, and more. 
+  Somewhere between 5-10% of the world’s economy is given to banks. In exchange they allow commerce at scale; international money exchanges, acts as a trusted third party, loans, basic financial services, and more. 
   </q-card-section>
 </q-card>
 
@@ -105,7 +98,7 @@ Somewhere between 5-10% of the world’s economy is given to banks. In exchange 
   <div class="text-subtitle2">8</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Without the internet this price tag is a bargain. However, in today’s world it is about 3-10% too high. More so, decades of clear disregard for structural stability, basic economic security, high fees to users, and constant predatory practices make a strong case for investments in alternatives.
+  Without the internet this price tag is a bargain. However, in today’s world it is about 3-10% too high. More so, decades of clear disregard for structural stability, basic economic security, high fees to users, and constant predatory practices make a strong case for investments in alternatives.
   </q-card-section>
 </q-card>
 
@@ -117,7 +110,7 @@ Without the internet this price tag is a bargain. However, in today’s world it
   <div class="text-subtitle2">9</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Bitcoin (2009) is the genesis blockchain. It combined advances in cryptography to create the first stable peer to peer digital currency. Tight, controlled, and led by a conservative development team, it seems to aim as a beacon of stability. The ultimate deflationary asset. 
+  Bitcoin (2009) is the genesis blockchain. It combined advances in cryptography to create the first stable peer to peer digital currency. Tight, controlled, and led by a conservative development team, it seems to aim as a beacon of stability. The ultimate deflationary asset. 
   </q-card-section>
 </q-card>
 
@@ -142,7 +135,7 @@ Bitcoin (2009) is the genesis blockchain. It combined advances in cryptography t
   <div class="text-subtitle2">11</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Forgoing a dramatic event, the future of blockchain will be built upon these two empires: The bitcoin empire of deflation in which all economies are pegged to, and the ethereum empire of web3 which allows for ownership of digital assets.
+  Forgoing a dramatic event, the future of blockchain will be built upon these two empires: The bitcoin empire of deflation in which all economies are pegged to, and the ethereum empire of web3 which allows for ownership of digital assets.
   </q-card-section>
 </q-card>
 
@@ -154,7 +147,7 @@ Forgoing a dramatic event, the future of blockchain will be built upon these two
   <div class="text-subtitle2">12</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-The grass is always greener on the other side. All forms of organization will have advantages and disadvantages. However, after some thousands of years of varying degrees of centralization it is pleasantly refreshing to see the rise of censorship resistant technologies. 
+  The grass is always greener on the other side. All forms of organization will have advantages and disadvantages. However, after some thousands of years of varying degrees of centralization it is pleasantly refreshing to see the rise of censorship resistant technologies. 
   </q-card-section>
 </q-card>
 
@@ -166,7 +159,7 @@ The grass is always greener on the other side. All forms of organization will ha
   <div class="text-subtitle2">13</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-A non-fungible token (NFT) is a token in a series in which each member is different. Humans are non-fungible members of the homo sapiens series, whereas the US dollar bills are fungible because they can be exchanged equally for one another. The homo sapien series is backed by methods of biology (decentralized) while the US dollar is backed by the government (centralized). 
+  A non-fungible token (NFT) is a token in a series in which each member is different. Humans are non-fungible members of the homo sapiens series, whereas the US dollar bills are fungible because they can be exchanged equally for one another. The homo sapien series is backed by methods of biology (decentralized) while the US dollar is backed by the government (centralized). 
   </q-card-section>
 </q-card>
 
@@ -178,7 +171,7 @@ A non-fungible token (NFT) is a token in a series in which each member is differ
   <div class="text-subtitle2">14</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-It depends on your definitions and which blockchain your NFT is on. In most cases the image itself is stored with an IPFS (which is semi-decentralized) and not on the blockchain. The NFT is a smart contract that is stored on a blockchain; some blockchains are more decentralized than others. 
+  It depends on your definitions and which blockchain your NFT is on. In most cases the image itself is stored with an IPFS (which is semi-decentralized) and not on the blockchain. The NFT is a smart contract that is stored on a blockchain; some blockchains are more decentralized than others. 
   </q-card-section>
 </q-card>
 
@@ -190,7 +183,7 @@ It depends on your definitions and which blockchain your NFT is on. In most case
   <div class="text-subtitle2">15</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Crypto Ancients are for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you devote your life to progress the world.
+  Crypto Ancients are for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you devote your life to progress the world.
   </q-card-section>
 </q-card>
 
@@ -202,7 +195,7 @@ Crypto Ancients are for those passionate to improve the world. People, artists, 
   <div class="text-subtitle2">16</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Humanity does not have infinite time to get our shit together. There are many internal and external existential threats that could end the human project before we have a chance to really explore nature. Greed, hatred, environmental destructuction, and more are real threats to progress and the long-term joy of our species. 
+  Humanity does not have infinite time to get our shit together. There are many internal and external existential threats that could end the human project before we have a chance to really explore nature. Greed, hatred, environmental destructuction, and more are real threats to progress and the long-term joy of our species. 
   </q-card-section>
 </q-card>
 
@@ -214,7 +207,7 @@ Humanity does not have infinite time to get our shit together. There are many in
   <div class="text-subtitle2">17</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-They do not. Crypto ancients consists of a few hundred layers made in photoshop, had a JS program rearrange into 10,000 unique Ancients, uploaded to pinata IPFS, pointed a Ethereum smart contract to those images, built a website, and made that contract available for minting. In most cases that is every NFT project today, give or take some bells and whistles. 
+  They do not. Crypto ancients consists of a few hundred layers made in photoshop, had a JS program rearrange into 10,000 unique Ancients, uploaded to pinata IPFS, pointed a Ethereum smart contract to those images, built a website, and made that contract available for minting. In most cases that is every NFT project today, give or take some bells and whistles. 
   </q-card-section>
 </q-card>
 
@@ -226,7 +219,7 @@ They do not. Crypto ancients consists of a few hundred layers made in photoshop,
   <div class="text-subtitle2">18</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Not at all! NFTs are creating the foundations for digital ownership. While Ethereum and other L1 blockchains created the methods. The actual pathway to ownership is defined by constant conflict among all the members and eventual agreement as to what ‘owning’ means. 
+  Not at all! NFTs are creating the foundations for digital ownership. While Ethereum and other L1 blockchains created the methods. The actual pathway to ownership is defined by constant conflict among all the members and eventual agreement as to what ‘owning’ means. 
   </q-card-section>
 </q-card>
 
@@ -238,14 +231,13 @@ Not at all! NFTs are creating the foundations for digital ownership. While Ether
   <div class="text-subtitle2">19</div>
   </q-card-section>
   <q-card-section class="q-pt-none">
-Digital spaces protect the environment. They allow us to meet and explore without having to destroy the life which surrounds us. Our connection to other lifeforms on the planet is vital to health and joy. There are now so many humans with such destructive machinery that we really must take the lead in respecting the environment around us. 
+  Digital spaces protect the environment. They allow us to meet and explore without having to destroy the life which surrounds us. Our connection to other lifeforms on the planet is vital to health and joy. There are now so many humans with such destructive machinery that we really must take the lead in respecting the environment around us. 
   </q-card-section>
 </q-card>
 
-  </div>
-  </div>
-  </div>
-
+</div>
+</div>
+</div>
 </template>
 
 <script>
@@ -256,12 +248,9 @@ export default defineComponent({
 </script>
 
 <style lang="sass" scoped>
-.my-card
-  width: 100%
-  height: 100%
-  max-width: 350px
-
-.background-image
+.sam-image
+  //WHY DOES STICKY NOT WORK FOR SUN ONLY IT EITHER WORKS FOR ALL OR NOTHING
+  position: abosolute
   position: sticky
   top: 0
   background-image: url(~assets/vueBackgroundSun.png)
@@ -269,6 +258,13 @@ export default defineComponent({
   // width: 100vh
   background-repeat: no-repeat
   background-size: contain  
+
+.my-card
+  width: 100%
+  height: 100%
+  max-width: 350px
+
+
 
 
 
