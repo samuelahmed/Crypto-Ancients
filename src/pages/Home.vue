d659fe2258133cabbb4a48dabffbf6fded3f6046
samuelahmed <samueladuval@gmail.com>
2022-02-08 00:04:28 -0800

    home updates

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="background-image" style="width: 100vh; height: 100vh; position: fixed"></div>
 
-  <div class="q-pa-md q-gutter-md stack-top">
+  <div class="">
     <div class="row justify-between">
 
 
-   <div class="q-pa-md">
+   <div class="q-pa-xl">
 <div class="row flex flex-center">
-  <div class="col-12 col-md-12 p-md">
+  <div class="col-12 col-md-12">
          <p class="fontchange" style="font-size: 100px; text-align: center;">
     CRYPTO ANCIENTS
         </p>
     <p class="fontchange" style="font-size: 28px; text-align: left;">
-    Crypto Ancients are for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you devote your life to progress the world.
+    Crypto Ancients is for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome.
     </p>
        <p class="fontchange" style="font-size: 28px; text-align: left;">
-    No roadmap or goals, the jpegs simply are themselves. NFTs living on the Ethereum blockchain hoping to inspire people to stand strong and make the hard decisions for the joy of our species.
+    No roadmap, the jpegs simply are themselves. NFTs living on the Ethereum blockchain hoping to inspire people to stand strong and make the hard decisions for the joy of our species.
     </p>
   </div>
 
