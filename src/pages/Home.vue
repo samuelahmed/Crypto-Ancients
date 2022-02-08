6de909f326275c98332a1e18232d9bd95d4e66fb
samuelahmed <samueladuval@gmail.com>
2022-02-07 18:41:24 -0800

    homepage updates

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,12 +7,15 @@
 
    <div class="q-pa-md">
 <div class="row flex flex-center">
-  <div class="col-12 col-md-6">
-         <p class="fontchange" style="font-size: 55px; text-align: center;">
+  <div class="col-12 col-md-12">
+         <p class="fontchange" style="font-size: 100px; text-align: center;">
     CRYPTO ANCIENTS
         </p>
-    <p class="fontchange" style="font-size: 20px; text-align: center;">
+    <p class="fontchange" style="font-size: 28px; text-align: left;">
     Crypto Ancients are for those passionate to improve the world. People, artists, leaders, workers, lovers, geniuses, degens and all are welcome so long as you devote your life to progress the world.
+    </p>
+       <p class="fontchange" style="font-size: 28px; text-align: left;">
+    No roadmap or goals, the jpegs simply are themselves. NFTs living on the Ethereum blockchain hoping to inspire people to stand strong and make the hard decisions for the joy of our species.
     </p>
   </div>
 
