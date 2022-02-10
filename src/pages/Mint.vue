c3b4625667e64fa8ceb32206815690bd2b54e040
samuelahmed <samueladuval@gmail.com>
2022-02-09 17:48:45 -0800

    mint updates

--- src/pages/Mint.vue
+++ src/pages/Mint.vue
@@ -3,33 +3,17 @@
 <div class="q-mt-xl">
 
   <div class="flex flex-center">
-<p class="fontchange q-mt-xl" style="font-size: 48px; text-align: center;">
+<p class="fontchange flex flex-center q-mt-xl" style="font-size: 48px; text-align: center;">
 Mint
 </p>
 </div> 
 <q-page class="flex-center">
 <div div id="q-app">
-<div class="q-pa-xl row items-start q-gutter-md">
+<div class="q-pa-xl row items-start q-gutter-md flex flex-center">
 <q-card class="my-card" flat bordered>
 <q-card-section horizontal>
-<q-card-section class="q-pt-md">
-<!-- <div class="text-h5 q-mt-sm q-mb-xs" style="text-align: center">
-Mint
-</div> -->
-<div class="text-caption text-grey">
-
-
-
-  Starting sometime soon you can mint your own Crypto Ancient. Proudly display it as your new profile picture to show your values and membership in the foundation. While you are allowed to mint up to 69 per address, please only hold a single one by 2025 to make sure our foundation is as vibrant and diverse as possible. When all have been minted the yellow button will disappear.
-  Each is 69x69 pixels and provides foundation membership to owner
-  Hot on the blockchain and in your collection
-  Minting requires Metamask, join the web3 revolution!
-  Minting creates and automatically transfers to you a single random Crypto Ancient NFT
-  Have full ownership of your Crypto Ancient and 0% Creator Royalty
-  Currently only Live on Rinkeby Test Network
-</div>      
-</q-card-section>
-<q-card-section class="col-4 flex flex-center">
+  <div class="flex flex-centered">
+  <q-card-section class="col-4 flex flex-center">
 <q-img
   class="rounded-borders"
   src="~assets/378.png"
@@ -37,14 +21,41 @@ Mint
   >
 </q-img>
 </q-card-section>
+<q-card-section class="q-pt-md">
+<!-- <div class="text-h5 q-mt-sm q-mb-xs" style="text-align: center">
+Mint
+</div> -->
+<div class="text-caption">
+ <div class="col-12 col-md-10">
+    <p class="fontchange q-mt-xl" style="font-size: 20px; text-align: left;">
+    There are 10,000 Crypto Ancients 
+    </p>
+       <p class="fontchange q-mt-lg" style="font-size: 20px; text-align: left;">
+      Each is unique 69x69 pixel art
+        </p>
+         <p class="fontchange q-mt-lg" style="font-size: 20px; text-align: left;">
+      Full ownership with 0% Creator Royalty
+    </p>
+      <p class="fontchange q-mt-lg" style="font-size: 20px; text-align: left;">
+      Mint yours for 0.01 ETH
+    </p>
+  </div>
+
+<div class="flex flex-center">
+<q-btn align="right" id="enableEthereumButton" color="primary">
+  Mint
+</q-btn>
+</div>
+ 
+</div>      
+</q-card-section>
+</div>
 </q-card-section>
 <q-separator>
 </q-separator>
-<q-card-actions>
-<q-btn id="enableEthereumButton" flat color="primary">
-  Mint
-</q-btn>
-</q-card-actions>
+<!-- <q-card-actions>
+
+</q-card-actions> -->
 </q-card>
 
 </div>
