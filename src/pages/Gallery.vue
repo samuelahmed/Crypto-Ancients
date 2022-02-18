72be6c525c3920ace070a823cebc7c6b85334273
samuelahmed <samueladuval@gmail.com>
2022-02-18 11:57:49 -0800

    draft moving backgrounds push to master

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,7 +1,86 @@
 <template>
+
+ <Particles
+    id="tsparticles"
+    :options="{
+      fpsLimit: 60,
+      interactivity: {
+        detectsOn: 'window',
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'push'
+          },
+          onHover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          resize: true
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.8,
+            size: 40,
+            speed: 3
+          },
+          push: {
+            quantity: 4
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: '#FB48C4'
+        },
+        links: {
+          color: '#FB48C4',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        collisions: {
+          enable: true
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outMode: 'bounce',
+          random: false,
+          speed: 6,
+          straight: false
+        },
+        number: {
+          density: {
+            enable: true,
+            value_area: 800
+          },
+          value: 80
+        },
+        opacity: {
+          value: 0.5
+        },
+        shape: {
+          type: 'circle'
+        },
+        size: {
+          random: true,
+          value: 5
+        }
+      },
+      detectRetina: true
+    }"
+  />
     <div class="background-image" style="width: 100vh; height: 100vh; position: fixed"></div>
 
   <div class="q-mt-xl">
+    
     <div class="flex flex-center">
      <p class="fontchange q-mt-xl" style="font-size: 48px; text-align: center;">
       Gallery
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,8 +11,89 @@
   </Renderer>
 </div> -->
 
+ <Particles
+    id="tsparticles"
+    :options="{
+      fpsLimit: 60,
+      interactivity: {
+        detectsOn: 'window',
+        events: {
+          onClick: {
+            enable: true,
+            mode: 'push'
+          },
+          onHover: {
+            enable: true,
+            mode: 'repulse'
+          },
+          resize: true
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.8,
+            size: 40,
+            speed: 3
+          },
+          push: {
+            quantity: 4
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: '#FFD700'
+        },
+        links: {
+          color: '#FFD700',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1
+        },
+        collisions: {
+          enable: true
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outMode: 'bounce',
+          random: false,
+          speed: 6,
+          straight: false
+        },
+        number: {
+          density: {
+            enable: true,
+            value_area: 800
+          },
+          value: 80
+        },
+        opacity: {
+          value: 0.5
+        },
+        shape: {
+          type: 'circle'
+        },
+        size: {
+          random: true,
+          value: 5
+        }
+      },
+      detectRetina: true
+    }"
+  />
+
     <div class="background-image" style="width: 100vh; height: 100vh; position: fixed"></div>
     
+
+
+
     <div class="q-mt-xl">
 
 
@@ -177,8 +258,6 @@ name: 'PageIndex',
     font-family: 'Titillium Web'
 
 .background-image
-  //WHY DOES STICKY NOT WORK FOR SUN ONLY IT EITHER WORKS FOR ALL OR NOTHING
-
   background-image: url(~assets/sunGolden.png)
   // height: 100vh
   // width: 100vh
