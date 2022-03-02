8dbe8cf30b59229cba9924176f77d295b1bdac97
samuelahmed <samueladuval@gmail.com>
2022-03-02 11:47:52 -0800

    added new imgs

---
+++ src/pages/placeholder.vue
@@ -0,0 +1,13 @@
+imgComp
+},
+data() {
+  return {
+    items: [],
+    show: true
+    }},
+  methods: {
+    onLoad()  { 
+    for (let i = 0; i < 10001; i++)    
+    this.items = i;
+    }}}
+    
