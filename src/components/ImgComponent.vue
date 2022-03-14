06fc27ebeb5e10c97b46a07481d79d5e04b2f350
samuelahmed <samueladuval@gmail.com>
2022-03-14 03:25:40 -0700

    added cards on gallery imgcomp

--- src/components/ImgComponent.vue
+++ src/components/ImgComponent.vue
@@ -1,11 +1,11 @@
 <template>
 <q-card
-class="card text-black q-ma-md"
-style="height: 300px; width: 300px"
+class="card text-black q-ma-sm"
+style="height: 325px; width: 250px"
 >
-  <q-card-section class="q-pt-none">
+  <q-card-section class="q-pa-none">
     <div class="q-pa-sm final">
-      <div class="flex flex-center q-pr-sm">
+      <div class="flex flex-center q-pr-sm text-h6">
       {{ edition }}
       </div> 
       <div class="flex flex-center">
@@ -15,16 +15,21 @@ style="height: 300px; width: 300px"
           :src="getImage()"
         />   
       </div>
-      <div class="flex flex-center q-pb-sm"> 
-          <div class="row">
-            <div class="col-6">
-              <p>{{ attributes[0].trait_type }}: {{ attributes[0].value }}</p> 
+
+      <div class="flex flex-center q-pt-sm text-h7">
+          {{ attributes[0].trait_type }}: {{ attributes[0].value }}
+      </div> 
+
+      <div class="q-pt-sm"> 
+          <div class="row text-center">
+            <div class="col-6 text-left q-pl-md">
               <p>{{ attributes[1].trait_type }}: {{ attributes[1].value }}</p> 
               <p>{{ attributes[3].trait_type }}: {{ attributes[3].value }}</p> 
               <p>{{ attributes[4].trait_type }}: {{ attributes[4].value }}</p> 
-            </div>
-            <div class="col-6">
               <p>{{ attributes[5].trait_type }}: {{ attributes[5].value }}</p> 
+
+            </div>
+            <div class="col-6 text-left q-pl-sm">
               <p>{{ attributes[6].trait_type }}: {{ attributes[6].value }}</p> 
               <p>{{ attributes[7].trait_type }}: {{ attributes[7].value }}</p> 
               <p>{{ attributes[8].trait_type }}: {{ attributes[8].value }}</p> 
@@ -74,12 +79,7 @@ export default defineComponent({
 <style>
 
 
-.card {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-}
+
 </style>
 
 
