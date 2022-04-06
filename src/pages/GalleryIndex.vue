59539d05f3e916ecd429e8f43858cd3d43e8b329
samuelahmed <samueladuval@gmail.com>
2022-04-06 16:34:54 -0700

    Fixed values & code cleanup: Wed Apr  6 16:34:49 PDT 2022

--- src/pages/GalleryIndex.vue
+++ src/pages/GalleryIndex.vue
@@ -4,48 +4,25 @@
     <div class="q-mt-xl text-center">
       <p class="text-h4">Attributes</p>
     </div>
-    <div  class="q-pt-sm" style="width: 17vw; min-width: 200px">
-        <!-- <q-badge color="secondary" class="q-mb-md">
-          Selected: {{ (
-            modelBackground + modelSun + modelEyes + modelFace + modelGlasses + modelHead +
-            modelJewelry + modelSkin + modelLips
-          ) }}
-        </q-badge>
-        <div> see the {{ filter }}</div> -->
-                <div class="q-pt-none" style="width: 17vw; min-width: 200px">
-              <q-select
+    <div class="q-pt-none" style="width: 17vw; min-width: 200px">
+      <q-select
         hide-dropdown-icon
-
-      filled
-      v-model="modelEdition"
-      use-input
-      use-chips
-      multiple
-      max-values="1"
-      input-debounce="0"
-      @new-value="createValue"
-      @filter="filterFn"
-      label="Search by ID">
-
-
-      <template v-slot:append>
+        filled
+        v-model="modelEdition"
+        use-input
+        use-chips
+        multiple
+        max-values="1"
+        input-debounce="0"
+        @new-value="createValue"
+        @filter="filterFn"
+        label="Search by ID">
+        <template v-slot:append>
           <q-icon name="search" @click.stop />
         </template>
-    </q-select>
-  
-      <!-- <q-select
-        v-model="modelEdition"
-        multiple
-        bg-color="white"
-        outlined
-        :options="optionsEdition"
-        use-chips
-        max-values="1"
-        label="Test"
-      /> -->
+      </q-select>
     </div>
-                    <div class="q-pt-sm" style="width: 17vw; min-width: 200px">
-
+    <div class="q-pt-sm" style="width: 17vw; min-width: 200px">
       <q-select
         v-model="modelBackground"
         multiple
@@ -61,7 +38,6 @@
         max-values="1"
         label="Background"
       />
-    </div>
     </div>
     <div class="q-pt-sm" style="width: 17vw; min-width: 200px">
       <q-select
@@ -187,19 +163,6 @@
                 :pagination="{rowsPerPage: 100}"
                 :rows-per-page-options="[50, 100, 250, 500, 1000]"
               >
-                <!-- <template v-slot:top-left>
-                  <q-input
-                    outlined
-                    dense
-                    debounce="300"
-                    v-model="filter"
-                    placeholder="Search"
-                    >
-                    <template 
-                      v-slot:append>
-                    </template>
-                  </q-input>
-                </template> -->
                 <template v-slot:item="props">
                   <div class="q-pa-sm">
                     <q-card style="width: 100px; height: 115px">
@@ -231,14 +194,9 @@
 import { defineComponent } from 'vue'
 import TableImgAncient from '../components/TableImgAncient.vue'
 import { ref } from 'vue'
-import FiltersVue from 'src/components/Filters.vue'
 
 const stringOptions = []
-
-
-
 const filterOptions = ref(stringOptions)
-
 const columns = [
   {
     name: 'edition',
@@ -271,15 +229,12 @@ const columns = [
   },
 ]
 
-// const thisValue = this.val
-
 export default defineComponent({
   name: 'GalleryIndex',
   components: {
     TableImgAncient,
   },
 setup () {
-
     return {
       modelTest: ref([]),
       modelBackground: ref([]),
@@ -294,8 +249,7 @@ setup () {
       modelEdition: ref([]),
       filterOptions,
       needle: ref(''),
-            model: ref(null),
-
+      model: ref(null),
 
       optionsBackground: [
         'Daytime', 'Night', 'Ocean', 'Metaverse', 'Forest'
@@ -313,44 +267,23 @@ setup () {
         'Green', 'Blue', 'Dark Blue', 'Dark Purple', 'Brown', 'Teal', 'Purple', 'Dark Red', 'Metaverse Blue', 'Beige', 'Orange', 'Dark Purple', 'Red'
       ],
       optionsFace: [
-        'face Standard', 'Curly Mustache', 'Heaven Sign', 'Numbers Tattoo', 'Seven Tattoo', 'Mask', 'Pink Circle', 'Heart Cheek', 'Stubble', 'Young Beard', 'Green Handlebars', 'Grey Beard'
+        'Standard', 'Curly Mustache', 'Heaven Sign', 'Numbers Tattoo', 'Seven Tattoo', 'Mask', 'Pink Circle', 'Heart Cheek', 'Stubble', 'Young Beard', 'Green Handlebars', 'Grey Beard'
       ],
       optionsGlasses: [
-        'glasses None', 'Fashion', 'Eye Patch', 'AR Headwear 2027', 'Future', 'Seeing', 'Active Red', 'Sunglasses', 'glasses Purple'
+        'None', 'Fashion', 'Eye Patch', 'AR Headwear 2027', 'Future', 'Seeing', 'Active Red', 'Sunglasses', 'Purple'
       ],
       optionsJewelry: [
-        'jewelry None', 'Facechain', 'Neck Swag Two', 'Green Neck Swag', 'Golden Round Earing', 'Metaverse Blue Gold', 'Gold Cross', 'Purple Gem', 'Pearls', 'Multiple Gold Earrings', 'Scarab Gold Plate', 'Gold Chain', 'Gold Earing', 'Neck Swag Unique', 'Neck Swag' 
+        'None', 'Facechain', 'Neck Swag Two', 'Green Neck Swag', 'Golden Round Earing', 'Metaverse Blue Gold', 'Gold Cross', 'Purple Gem', 'Pearls', 'Multiple Gold Earrings', 'Scarab Gold Plate', 'Gold Chain', 'Gold Earing', 'Neck Swag Unique', 'Neck Swag' 
       ],
       optionsHead: [
-        'head Standard', 'Hardhat', 'Metapriest', 'head Queen', 'Metaverse Queen', 'Ancient Headwear', 'Gold Hat', 'head Blue Hair', 'head Purple Hair', 'Crazy Blue Hair', 'Crazy Green Hair', 'Crazy Grey Hair', 'Side Ponytail', 'Orange Bun', 'head Grey Hair', 'Purple Blue Bangs', 'Flow Hair',
+        'Standard', 'Hardhat', 'Metapriest', 'Queen', 'Metaverse Queen', 'Ancient Headwear', 'Gold Hat', 'Blue Hair', 'Purple Hair', 'Crazy Blue Hair', 'Crazy Green Hair', 'Crazy Grey Hair', 'Side Ponytail', 'Orange Bun', 'Grey Hair', 'Purple Blue Bangs', 'Flow Hair',
         'Brown Punk Hair', 'Metaverseblue Punk Hair', 'Gold Punk Hair', 'Multicolor Hat', 'Blue Hat', 'Animal Ears', 'Pink Hoodie', 'Grey Hoodie', 'Underground Pink', 'NPC Purple', 'NPC Brown', 'Lego Orange', 'Lego Brown', 'Pink Killa', 'Curly Purple Hair', 'Napoleon Hat'
       ],
-      // optionsEdition: [
-      //   '1', '69', '2356', '6529', '99999'
-      // ],
-      
-      
       createValue (val, done) {
-        // Calling done(var) when new-value-mode is not set or "add", or done(var, "add") adds "var" content to the model
-        // and it resets the input textbox to empty string
-        // ----
-        // Calling done(var) when new-value-mode is "add-unique", or done(var, "add-unique") adds "var" content to the model
-        // only if is not already set
-        // and it resets the input textbox to empty string
-        // ----
-        // Calling done(var) when new-value-mode is "toggle", or done(var, "toggle") toggles the model with "var" content
-        // (adds to model if not already in the model, removes from model if already has it)
-        // and it resets the input textbox to empty string
-        // ----
-        // If "var" content is undefined/null, then it doesn't tampers with the model
-        // and only resets the input textbox to empty string
-
-      // console.log(val)
         if (val.length > 0) {
           if (!stringOptions.includes(val)) {
             stringOptions.push(val)
           }
-
           done(val, 'toggle')
         }
       },
@@ -359,15 +292,11 @@ setup () {
           if (val === '') {
             filterOptions.value = stringOptions
           }
-   
           else {
             const needle = val.toLowerCase()
             filterOptions.value = stringOptions.filter(
               v => v.toLowerCase().indexOf(needle) > -1
             )
-                          // console.log(needle)
-                          // console.log(filterOptions.value)
-
           }
         })
       }
@@ -381,8 +310,6 @@ setup () {
     rows: [],
     Selected: [],
     list: [],
-    needle: '',
-
   }),
   async created () {
     try {
@@ -407,7 +334,6 @@ setup () {
       this.$router.push({ name: 'AncientDetails', params: { edition } })
     },
     buildFilter() {
-      //Partially repairs q-table pagination by preventing filter from activating until a value is selected.
       //STILL BROKEN IF VALUE IS SELECTED THAT HAS MORE THAN ONE PAGE
       if (
         this.modelBackground.length > 0 ||
@@ -447,73 +373,13 @@ setup () {
         }
       )
     },
-     numberSearchFilter(rows, terms, cols, cellValue) {
-        console.log(filterOptions.value)
+    numberSearchFilter(rows, terms, cols, cellValue) {
       const lowerTerms = filterOptions.value[filterOptions.value.length - 1]
-              console.log(lowerTerms)
       return rows.filter(
         row => cols.some(col => (cellValue(col, row)).toLowerCase() === lowerTerms))
-      
-    },
-  }})
-
-
-
-
-       //row.editon is the number of the edition
-      //          console.log(filterOptions.value)
-      //          console.log(row.edition)
-
-      // return rows.filter(
-      //   row => { 
-          // for (var e=0; e<=10000; e++){ 
-            // if (filterOptions.value == row.edition)
-            
-            //   return false
-          // for (row.edition; row.edition <= 10000; row.edition++) { 
-          // if (filterValues.length && 
-          //     !filterValues.some(filterValue => (row.edition).toString().indexOf(filterValue) > -1))
-          // }
-      //     return true;
-      //     }
-      //   )
-      // },
-
-
-      // return rows.filter(
-      //   row => { 
-      //     for (var i=0;i<=9;i++){ 
-      //       if (filterValues[i].length && 
-      //         !filterValues[i].some(filterValue => (row.attributes[i]).value.toLowerCase().includes(filterValue.toLowerCase())))
-      //         return false;
-      //     }
-      //     return true;
-      //   }
-      // )
-  
-
-                  // row.edition.toString().includes(val)
-
-         // else if (i == 10) {
-            //     filterValue => (row.edition).value.toLowerCase().includes(filterValue.toLowerCase())
-            //   return false;
-            // }
-          //row.edition
-        // for (var j=0; j<=10000; j++){
-        //   if (filterValues[j].length &&
-        //     !filterValues[j].some(filterValue => (row.edition).value.toLowerCase().includes(filterValue.toLowerCase())))
-        //     return false;
-        // }
-              // console.log('bailed at i:'+i+' for:'+row.name)
-                          // console.log(row.attributes[1])
-
-      //                             let lowerTerms = this.new-value.toLowerCase()
-      //   let filteredRows = rows.filter(
-      //     row => toFilter.some(col => (row[col] + '').toLowerCase().includes(lowerTerms))
-      //   )
-      // return filteredRows
-
-
+    }
+  }
+})
 </script>
 
 
